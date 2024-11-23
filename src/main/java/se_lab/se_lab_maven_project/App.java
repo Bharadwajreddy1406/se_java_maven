b24dc4d00fea511769a4445034eb06775a974976
Bharadwajreddy1406 <bharadwajreddy146@gmail.com>
2024-11-23 11:52:46 +0530

    Added files

---
+++ src/main/java/se_lab/se_lab_maven_project/App.java
@@ -0,0 +1,13 @@
+package se_lab.se_lab_maven_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
